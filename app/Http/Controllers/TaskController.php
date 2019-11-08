a8ecef8f8aa703e00e521637b85249e239b9c1c4
sktama <skodama@sejuku.net>
2019-11-08 15:57:16 +0900

    task表示領域追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,16 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Folder;
+use App\Task;
 
 class TaskController extends Controller
 {
   public function index(int $id)
   {
+    // 全てのフォルダーを取得する
     $folders = Folder::all();
 
+    // 選択されたフォルダーを取得する
+    $current_folder = Folder::find($id);
+
+    // 選択されたフォルダーに紐付くタスクを取得する
+    $tasks = Task::where('folder_id', $current_folder->id)->get();
+
+    // 一覧を描画する際に値を一緒に送信
     return view('tasks/index', [
       'folders' => $folders,
       'current_folder_id' => $id,
+      'tasks' => $tasks,
     ]);
   }
 }
