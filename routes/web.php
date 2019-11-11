7f2c6349dba74f2b5b13a08cef8a7e863dd5acad
sktama <skodama@sejuku.net>
2019-11-11 23:08:41 +0900

    ユーザ登録画面テンプレート作成

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
+Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/folders/{id}/tasks', 'TaskController@index') -> name('tasks.index');
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
