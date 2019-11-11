4e2b1bd187bc91fbb503bbdcd4587d65b11609cd
sktama <skodama@sejuku.net>
2019-11-11 09:58:12 +0900

    タスク編集画面実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -52,4 +52,16 @@ class TaskController extends Controller
       'id' => $current_folder->id,
     ]);
   }
+
+  /**
+   * GET /folders/{id}/tasks/{task_id}/edit
+   */
+  public function showEditForm(int $id, int $task_id)
+  {
+      $task = Task::find($task_id);
+
+      return view('tasks/edit', [
+          'task' => $task,
+      ]);
+  }
 }
