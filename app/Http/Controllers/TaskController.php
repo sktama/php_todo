c5ac96d40b683c5751ed193be451b06d2ca7951a
sktama <skodama@sejuku.net>
2019-11-11 14:07:44 +0900

    テストコード実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Folder;
 use App\Task;
 use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
 
 class TaskController extends Controller
 {
@@ -58,26 +59,26 @@ class TaskController extends Controller
    */
   public function showEditForm(int $id, int $task_id)
   {
-      $task = Task::find($task_id);
+    $task = Task::find($task_id);
 
-      return view('tasks/edit', [
-          'task' => $task,
-      ]);
+    return view('tasks/edit', [
+        'task' => $task,
+    ]);
   }
+
   public function edit(int $id, int $task_id, EditTask $request)
   {
       // 1
       $task = Task::find($task_id);
-
       // 2
       $task->title = $request->title;
       $task->status = $request->status;
       $task->due_date = $request->due_date;
       $task->save();
-
       // 3
       return redirect()->route('tasks.index', [
           'id' => $task->folder_id,
       ]);
   }
+
 }
