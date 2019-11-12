5c2fe18bf15283ba5a8ef7006c81b0968bc6043e
sktama <skodama@sejuku.net>
2019-11-12 15:12:05 +0900

    ユーザに紐づくフォルダの作成処理実装

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateFolder;
 use App\Folder;
+// ★ Authクラスをインポートする
+use Illuminate\Support\Facades\Auth;
 
 class FolderController extends Controller
 {
@@ -19,8 +21,8 @@ class FolderController extends Controller
         $folder = new Folder();
         // タイトルに入力値を代入する
         $folder->title = $request->title;
-        // インスタンスの状態をデータベースに書き込む
-        $folder->save();
+        // ★ ユーザーに紐づけて保存
+        Auth::user()->folders()->save($folder);
 
         return redirect()->route('tasks.index', [
             'id' => $folder->id,
