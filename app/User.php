84eb662c2469f19780e64331ce1f0df0007e0937
sktama <skodama@sejuku.net>
2019-11-12 17:40:40 +0900

    自作のパスワード再設定メールテンプレート適用

--- app/User.php
+++ app/User.php
@@ -5,6 +5,8 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Mail\ResetPassword; // ★ 追加
+use Illuminate\Support\Facades\Mail; // ★ 追加
 
 class User extends Authenticatable
 {
@@ -39,5 +41,13 @@ class User extends Authenticatable
     public function folders()
     {
         return $this->hasMany('App\Folder');
+    }
+
+    /**
+     * ★ パスワード再設定メールを送信する
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        Mail::to($this)->send(new ResetPassword($token));
     }
 }
