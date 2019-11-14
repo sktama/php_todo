83d778ddd407c8810dcc7537748e7f0f757b38e8
sktama <skodama@sejuku.net>
2019-11-14 17:34:29 +0900

    ポリシーの設定

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@
 
 Route::group(['middleware' => 'auth'], function() {
   Route::get('/', 'HomeController@index')->name('home');
-  Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
+  Route::group(['middleware' => 'can:view,folder'], function() {
+    Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
+  });
   Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
   Route::post('/folders/create', 'FolderController@create');
   Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
@@ -22,4 +24,5 @@ Route::group(['middleware' => 'auth'], function() {
   Route::get('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')->name('tasks.edit');
   Route::post('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
 });
+
 Auth::routes();
