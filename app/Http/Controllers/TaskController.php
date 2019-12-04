32dac778380972d884cb6183629bd3c400ac5a62
sktama <s.kodama0417@gmail.com>
2019-12-04 12:10:49 +0900

    デプロイ準備

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -71,6 +71,7 @@ class TaskController extends Controller
      */
     public function showEditForm(Folder $folder, Task $task)
     {
+        $this->checkRelation($folder, $task);
         return view('tasks/edit', [
             'task' => $task,
         ]);
@@ -85,6 +86,7 @@ class TaskController extends Controller
      */
     public function edit(Folder $folder, Task $task, EditTask $request)
     {
+        $this->checkRelation($folder, $task);
         $task->title = $request->title;
         $task->status = $request->status;
         $task->due_date = $request->due_date;
@@ -93,6 +95,13 @@ class TaskController extends Controller
         return redirect()->route('tasks.index', [
             'id' => $task->folder_id,
         ]);
+    }
+
+    private function checkRelation(Folder $folder, Task $task)
+    {
+        if ($folder->id !== $task->folder_id) {
+            abort(404);
+        }
     }
 
 }
